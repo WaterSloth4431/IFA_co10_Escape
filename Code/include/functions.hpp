ea17478d1c6c833f28a430e4cca2c54d5a4c304c
WaterSloth4431 <nwstoner42028@gmail.com>
2023-04-30 15:32:29 -0700

    added taw view distance script

--- Code/include/functions.hpp
+++ Code/include/functions.hpp
@@ -1,5 +1,26 @@
 class CfgFunctions
 {
+	class TAW_VD {
+	tag = "TAWVD";
+
+		class Initialize {
+			file = "taw_vd";
+			class stateTracker {
+				ext = ".fsm";
+				postInit = 1;
+				headerType = -1;
+			};
+				
+			class onSliderChanged {};
+			class onTerrainChanged {};
+			class updateViewDistance {};
+			class openMenu {};
+			class onChar {};
+			class openSaveManager {};
+			class onSavePressed {};
+			class onSaveSelectionChanged {};
+		};
+	};
 	class A3E
 	{
 		class Common
