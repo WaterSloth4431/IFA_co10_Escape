0a416985c32a521f0c7ec9092ed5a80fb6d89fdd
WaterSloth4431 <nwstoner42028@gmail.com>
2023-05-10 19:14:43 -0700

    added txu info

--- Code/include/functions.hpp
+++ Code/include/functions.hpp
@@ -1,5 +1,14 @@
 class CfgFunctions
 {
+	class TXU_INFO	// Tag
+	{
+	class f	// core functions
+	{
+		file = "TXU_INFO\f";
+		class Pre_Init			{description = "TXU_INFO initialisation function.";preInit = 1;};
+		class Post_Init			{description = "TXU_INFO initialisation function.";postInit = 1;};
+	};
+	};
 	class TAW_VD {
 	tag = "TAWVD";
 
